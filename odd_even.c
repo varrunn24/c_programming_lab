0b5b0e087df8430f296d22562d3290d2b1cb4597
varrunn24 <146822454+varrunn24@users.noreply.github.com>
2023-10-05 12:13:09 +0530

    Add files via upload

---
+++ odd_even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a ;
+    printf("enter any number \n");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("the number is even.");
+    }
+    else 
+    {
+        printf("the number is odd.");
+    }
+}
