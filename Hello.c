709b0e0d486671b85c1d87b704abc51cd51ef8e2
varrunn24 <146822454+varrunn24@users.noreply.github.com>
2023-10-06 13:31:50 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello world!");
+    //using tab character
+    printf("\nHello world ! \tHello World!");
+
+    //using the line character
+    printf("\nHello World!");
+    return 0;
+    
+}
